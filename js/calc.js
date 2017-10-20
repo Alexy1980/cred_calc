7cc6300cbf75098672b6532e2947569aef1a5532
Paul <test@mail.ru>
2017-10-20 22:27:39 +0400

    Angular JS slider complete

--- js/calc.js
+++ js/calc.js
@@ -1,39 +1,38 @@
-
-angular.module('creditCalc', []).controller('calcCtrl', function ($scope)
+angular.module('creditCalc', ['rzModule']).controller('calcCtrl', function ($scope)
 {
-
-    $scope.sumCredit = '';
-    $scope.timeCredit = '';
     $scope.percentRateCredit = '';
-
+    $scope.sumSlider = 1500000;
+    $scope.timeSlider = 256;
+    $scope.percentSlider = 100;
 
     $scope.action = function()
     {
-        $scope.timeStr = $scope.timeCredit + ' месяцев';
-        $scope.percentStr = $scope.percentRateCredit + ' %';
+        $scope.timeStr = $scope.timeSlider + ' месяцев';
+        $scope.percentStr = $scope.percentSlider + ' %';
         $scope.results = [];
         $scope.sumNa4islProcenti = 0;
         $scope.sumSumPlatej = 0;
-        $scope.sumSumCredit = 0;
+        $scope.sumSumSlider = 0;
 
         if( $scope.check() )
         {
             document.getElementById('res').style.display = 'table';
 
-            var zadoljnost = +$scope.sumCredit;
-            var timeCredit = +$scope.timeCredit;
-            var percentRateCredit = +$scope.percentRateCredit;
+            var zadoljnost = +$scope.sumSlider; // общая сумма кредита
+            var timeSlider = +$scope.timeSlider; // срок кредита
+            var percentRateCredit = +$scope.percentSlider; // процентная ставка в год
 
             $scope.sumNa4islProcenti = 0;
             $scope.sumSumPlatej = 0;
-            $scope.sumSumCredit = +$scope.sumCredit;
+            // Добавление знака + перед переменной (убедитесь, что между ними нет места) сообщает интерпретатору JavaScript, чтобы попытаться преобразовать строку в числовое значение - если строка содержит только цифры. Другой способ - использовать команду Number($scope.sumSlider)
+            $scope.sumSumSlider = +$scope.sumSlider;
 
-            var osnovnoiDolg = $scope.myRound(zadoljnost / timeCredit);
+            var osnovnoiDolg = $scope.myRound(zadoljnost / timeSlider); // фиксированная на весь период сумма, идущая на погашение суммы задолженности
 
             var na4islProcenti = 0;
             var sumPlatej = 0;
 
-            for ( var i = 1; i <= timeCredit; i++ )
+            for ( var i = 1; i <= timeSlider; i++ )
             {
                 na4islProcenti = $scope.myRound((zadoljnost / 100) * (percentRateCredit / 12));
                 sumPlatej = osnovnoiDolg + na4islProcenti;
@@ -62,34 +61,34 @@ angular.module('creditCalc', []).controller('calcCtrl', function ($scope)
         $scope.errors = {};
         var check = true;
 
-        var zadoljnost = +$scope.sumCredit;
-        var timeCredit = +$scope.timeCredit;
-        var percentRateCredit = +$scope.percentRateCredit;
+        var zadoljnost = +$scope.sumSlider;
+        var timeSlider = +$scope.timeSlider;
+        var percentRateCredit = +$scope.percentSlider;
 
         if ( zadoljnost == 0 ) {
-            $scope.errors.zadoljnost = 'Ошибка рассчета кредита.';
+            $scope.errors.zadoljnost = 'Поле не должно быть пустым.';
             check = false;
         }
         else if ( isNaN(zadoljnost) ) {
-            $scope.errors.zadoljnost = 'Ошибка рассчета кредита.';
+            $scope.errors.zadoljnost = 'Значение должно быть числом.';
             check = false;
         }
 
-        if ( timeCredit == 0 ) {
-            $scope.errors.timeCredit = 'Ошибка рассчета кредита.';
+        if ( timeSlider == 0 ) {
+            $scope.errors.timeSlider = 'Поле не должно быть пустым.';
             check = false;
         }
-        else if ( isNaN(timeCredit) ) {
-            $scope.errors.timeCredit = 'Ошибка рассчета кредита.';
+        else if ( isNaN(timeSlider) ) {
+            $scope.errors.timeSlider = 'Значение должно быть числом.';
             check = false;
         }
 
         if ( percentRateCredit == 0 ) {
-            $scope.errors.percentRateCredit = 'Ошибка рассчета кредита.';
+            $scope.errors.percentSlider = 'Поле не должно быть пустым.';
             check = false;
         }
         else if ( isNaN(percentRateCredit) ) {
-            $scope.errors.percentRateCredit = 'Ошибка рассчета кредита.';
+            $scope.errors.percentSlider = 'Значение должно быть числом.';
             check = false;
         }
 
@@ -102,3 +101,5 @@ angular.module('creditCalc', []).controller('calcCtrl', function ($scope)
     };
 
 });
+
+
